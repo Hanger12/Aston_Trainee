2802ade05ef91a34b811ec633fe3a3c0fc3c7f8b
Artem <killersuper1212@gmail.com>
2023-11-09 00:03:20 +0400

    init Project

---
+++ src/test/java/aston/aston_trainee/AstonTraineeApplicationTests.java
@@ -0,0 +1,13 @@
+package aston.aston_trainee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AstonTraineeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
